5366f1b736b826d2904149de983450c465f9f7c2
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-12-10 15:07:43 +0000

    Use jinja2 for templating

--- prototype.py
+++ prototype.py
@@ -4,21 +4,14 @@ import json
 import os
 import pymysql
 
-from pprint import pprint
 from boltons.strutils import slugify
+from jinja2 import Environment, FileSystemLoader
 
 db_host = os.environ['VRTRACK_HOST']
 db_port = int(os.environ['VRTRACK_PORT'])
 db_user = os.environ['VRTRACK_RO_USER']
 
 db_db = 'pathogen_prok_track'
-
-connection = pymysql.connect(
-    host=db_host,
-    port=db_port,
-    user=db_user,
-    db=db_db
-)
 
 query = """\
 SELECT DISTINCT latest_project.name,
@@ -46,6 +39,12 @@ WHERE           latest_lane.library_id = library.library_id AND
 output_dir = 'site/'
 
 def get_all_details(species_list):
+    connection = pymysql.connect(
+        host=db_host,
+        port=db_port,
+        user=db_user,
+        db=db_db
+    )
     with connection.cursor() as cursor:
         cursor.execute(query)
         details_list = cursor.fetchall()
@@ -60,32 +59,30 @@ def get_species_names(path):
         species = [line.strip() for line in species_file]
     return species
 
-def output_filename(species, output_dir):
-    return os.path.join(output_dir, slugify(species).lower()+'.json')
+def output_filename(species):
+    return slugify(species).lower()+'.json'
+
+def output_filepath(species, output_dir):
+    return os.path.join(output_dir, output_filename(species))
 
 def write_details(details, output_dir):
     for name, sub_details in details.items():
-        outpath = output_filename(name, output_dir)
+        outpath = output_filepath(name, output_dir)
         with open(outpath, 'w') as outfile:
             json.dump({name: sub_details}, outfile)
 
-def write_index(details, output_dir):
-    with open(os.path.join(output_dir, 'index.html'), 'w') as index_file:
-        print("""\
-<html>
-    <head>
-        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    </head>
-    <body>
-        <ul>
-""", file=index_file)
+def get_template():
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    templates_dir = os.path.join(script_dir, 'templates')
+    loader = FileSystemLoader(templates_dir)
+    env = Environment(loader=loader)
+    index_template = env.get_template('index.html')
+    return index_template
 
-        for name in sorted(details.keys()):
-            species_path=output_filename(name, output_dir)
-            species_relative_path=species_path.replace(output_dir, '')
-            print("            <li><a href='%s'>%s</a></li>" % (species_relative_path,
-                                                    name), file=index_file)
-        print("        </ul>\n    <body>", file=index_file)
+def write_index(details, output_dir):
+    species_urls = {name: output_filename(name) for name in details.keys()}
+    with open(os.path.join(output_dir, 'index.html'), 'w') as index_file:
+        print(get_template().render(species_urls=species_urls), file=index_file)
 
 #species = get_species_names('/nfs/pathogen/project_pages/Project_webpages_species_list_Prokaryotes.txt')
 species = get_species_names('Project_webpages_species_list_Prokaryotes.txt')
