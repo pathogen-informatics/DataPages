3fbee2fcc48d3972664e16606c66009dcbb97797
Ben Taylor <ben.taylor@sanger.ac.uk>
2016-02-10 16:04:25 +0000

    Fix whitespace

--- datapages/update_nctc.py
+++ datapages/update_nctc.py
@@ -7,7 +7,7 @@ import yaml
 
 from argparse import ArgumentTypeError, FileType
 
-from .common import cache_data, reload_cache_data,
+from .common import cache_data, reload_cache_data, \
                     _is_dir, _could_write, _could_read
 from .update_projects import get_template
 
@@ -329,9 +329,9 @@ def main():
     Domain specific config is always loaded from a config file"""
     args = parse()
     
-    from .regenerate_data import get_vrtrack_db_details_list,
-                                 get_sequencescape_db_details,
-                                 get_all_data,
+    from .regenerate_data import get_vrtrack_db_details_list, \
+                                 get_sequencescape_db_details, \
+                                 get_all_data, \
                                  _get_default_columns
 
     if args.quiet:
