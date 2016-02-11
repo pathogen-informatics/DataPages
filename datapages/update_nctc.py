e21a376a551472383e69c82b4818762d345029fd
Ben Taylor <ben.taylor@sanger.ac.uk>
2016-02-11 14:18:06 +0000

    Try to fix something wierd with paths

--- datapages/update_nctc.py
+++ datapages/update_nctc.py
@@ -54,7 +54,7 @@ def _parse_automatic_gffs(all_paths, root_dir, root_url):
     lookup = []
     file_regex = "([^/]+).gff$"
     path_regex = re.compile(os.path.join(root_dir, file_regex))
-    for path, match in ((path, path_regex.match(path)) for path in all_paths):
+    for path, match in ((_path, path_regex.match(_path)) for _path in all_paths):
         if match:
             sample_accession, = match.groups()
             url = _file_to_ftp_url(path, root_dir, root_url)
@@ -94,7 +94,7 @@ def _parse_manual_gffs(all_paths, root_dir, root_url):
     lookup = []
     file_regex = "([^/]+).gff$"
     path_regex = re.compile(os.path.join(root_dir, file_regex))
-    for path, match in ((path, path_regex.match(path)) for path in all_paths):
+    for path, match in ((_path, path_regex.match(_path)) for _path in all_paths):
         if match:
             sample_accession, = match.groups()
             url = _file_to_ftp_url(path, root_dir, root_url)
@@ -135,7 +135,7 @@ def _parse_manual_embls(all_paths, root_dir, root_url):
     lookup = []
     file_regex = "([^/]+).embl$"
     path_regex = re.compile(os.path.join(root_dir, file_regex))
-    for path, match in ((path, path_regex.match(path)) for path in all_paths):
+    for path, match in ((_path, path_regex.match(_path)) for _path in all_paths):
         if match:
             sample_accession, = match.groups()
             url = _file_to_ftp_url(path, root_dir, root_url)
